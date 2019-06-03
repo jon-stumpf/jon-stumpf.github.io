f6e8edb5117c1e165f57f3e5f763b875a3ffa92f
jon-stumpf <jon.stumpf@gmail.com>
2019-06-02 23:15:30 -0400

    Fixed typo

--- curtis.js
+++ curtis.js
@@ -102,7 +102,7 @@ windyInit( options, windyAPI => {
 
 		    markers.push(marker);
 		    marker._icon.setAttribute('data-heading', message.heading);
-		    marker.bringtoback();
+		    marker.bringToBack();
 
 		    updateIconStyle();
 		}
