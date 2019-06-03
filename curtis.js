76973cc0cf777f65fab2561fad602ad796cc43ea
jon-stumpf <jon.stumpf@gmail.com>
2019-06-02 22:48:54 -0400

    Reverting

--- curtis.js
+++ curtis.js
@@ -32,7 +32,7 @@ windyInit( options, windyAPI => {
 	iconUrl: MESSAGE_ICON_URL,
 	iconSize: [16, 16],
 	iconAnchor: [8, 8],
-	popupAnchor: [16, 16],
+	popupAnchor: [0, 0],
     });
 
     const markers = [];
