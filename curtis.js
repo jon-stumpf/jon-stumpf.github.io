5edcf74a1d36400ef2a426a78a83350b66ef7a32
jon-stumpf <jon.stumpf@gmail.com>
2019-06-02 23:23:43 -0400

    Removed markers[];

--- curtis.js
+++ curtis.js
@@ -35,20 +35,6 @@ windyInit( options, windyAPI => {
 	popupAnchor: [8, 16],
     });
 
-    const markers = [];
-
-    const updateIconStyle = () => {
-        for (const marker of markers) {
-            if (marker._icon) {
-		const heading = marker._icon.getAttribute('data-heading');
-		if (marker._icon.style.transform.indexOf('rotateZ') === -1) {
-		    marker._icon.style.transform = `${marker._icon.style.transform} rotateZ(${(heading || 0)}deg)`;
-		    marker._icon.style.transformOrigin = 'center';
-		}
-            }
-        }
-    };
-
     fetch("./messages.json")
         .then(response => response.json())
         .then(something => something.result)
@@ -75,7 +61,6 @@ windyInit( options, windyAPI => {
 				title: messageTime,
 				icon: MessageIcon,
 			    }).addTo(map);
-			    markers.push(marker);
 			    marker.bindPopup(message.text);
 			}
 
@@ -98,12 +83,16 @@ windyInit( options, windyAPI => {
 
 		    const marker = L.marker([message.lat_dec, message.lon_dec], {
 			icon: BoatIcon,
-		    }).bringToBack().addTo(map);
+		    });
 
-		    markers.push(marker);
-		    marker._icon.setAttribute('data-heading', message.heading);
+		    marker.bringToBack();
 
-		    updateIconStyle();
+		    if (marker._icon.style.transform.indexOf('rotateZ') === -1) {
+			marker._icon.style.transform = `${marker._icon.style.transform} rotateZ(${(message.heading || 0)}deg)`;
+			marker._icon.style.transformOrigin = 'center';
+		    }
+
+		    marker.addTo(map);
 		}
             } catch (error) {
 		console.error(`Error querying messages: ${error.message}`);
